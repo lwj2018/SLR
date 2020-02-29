36d65691e13f601d8f43309296277b14272d6492
lwj2018 <lwj19970331@gmail.com>
2020-02-29 14:59:35 +0800

    20200229 label smoothing

--- args.py
+++ args.py
@@ -20,7 +20,7 @@ class Arguments:
         self.num_classes = 500
         self.epochs = 100
         self.batch_size = 1
-        self.learning_rate = 1e-4
+        self.learning_rate = 1e-5
         self.sample_size = 128
         self.clip_length = 16
         self.drop_p = 0.0
